685c1cedcc5bfdccb5fe38fa239d4f7de44e6afe
alexandr <dmitrymaslov2016@mai.ru>
2023-09-14 21:07:04 +0300

    change Customuser

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from educational_modules.models import EducationalModule
+from users.manager import UserManager
 
 
 class CustomUser(AbstractUser):
@@ -12,4 +13,14 @@ class CustomUser(AbstractUser):
     educational_modules = models.ManyToManyField(EducationalModule)
 
     USERNAME_FIELD = 'email'
+
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
