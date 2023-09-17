22e23eadf5c78127aa5fbbbc8f7f3e2b773b0999
alexandr <dmitrymaslov2016@mai.ru>
2023-09-17 19:27:47 +0300

    change serializers.py ed_module

--- educational_modules/serializers.py
+++ educational_modules/serializers.py
@@ -6,19 +6,14 @@ from educational_modules.models import EducationalModule
 
 
 class EducationalModuleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = EducationalModule
-        fields = ('name', 'description', 'price', 'image')
-
-
-class EdModuleWithContentSerializer(serializers.ModelSerializer):
     content = SerializerMethodField()
-    def get_content(self, education_module):
-        queryset = Content.objects.filter(educational_module=education_module)
-        return queryset
 
+    def get_content(self, education_module):
+        queryset = [con.name for con in Content.objects.filter(educational_module=education_module)]
+        return queryset
     class Meta:
         model = EducationalModule
         fields = ('name', 'description', 'price', 'image', 'content')
+
+
 
