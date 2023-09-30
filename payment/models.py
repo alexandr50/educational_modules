42d51ad1af78c7c8f0ba38740584c0a0668ca8b1
alexandr <dmitrymaslov2016@mai.ru>
2023-09-30 16:01:07 +0300

    change model payment

--- payment/models.py
+++ payment/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Payment(models.Model):
     is_done = models.BooleanField(default=False, verbose_name='Оплачено')
-    user = models.OneToOneField('users.CustomUser',
+    user = models.ForeignKey('users.CustomUser',
                                 on_delete=models.CASCADE,
                                 verbose_name='покупатель')
-    educational_module = models.OneToOneField('educational_modules.EducationalModule',
+    educational_module = models.ForeignKey('educational_modules.EducationalModule',
                                               on_delete=models.CASCADE,
                                               verbose_name='Образовательный модуль')
 
